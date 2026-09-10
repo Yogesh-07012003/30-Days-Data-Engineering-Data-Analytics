4c27e9b8bdafc6e15664580ee4bd883990477b96
Yogesh-07012003 <146987985+Yogesh-07012003@users.noreply.github.com>
2026-09-10 11:29:07 +0530

    Create data_types.py

---
+++ Day-01/data_types.py
@@ -0,0 +1,18 @@
+name = "Yogesh"          # String
+age = 23                 # Integer
+salary = 25000.50        # Float
+is_student = False       # Boolean
+
+skills = ["Python", "SQL", "Power BI"]  # List
+
+person = {
+    "name": "Yogesh",
+    "role": "Data Engineer"
+}  # Dictionary
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(is_student))
+print(type(skills))
+print(type(person))
