e864bf5819d49fd71e39b9bf610679bc5d6d4cf9
Yogesh-07012003 <146987985+Yogesh-07012003@users.noreply.github.com>
2026-09-11 10:00:11 +0530

    Create tuples.py

---
+++ Day-02/tuples.py
@@ -0,0 +1,15 @@
+# Creating a tuple
+employee = ("Yogesh", 24, "Data Engineer")
+
+print(employee)
+
+# Accessing elements
+print(employee[0])
+print(employee[2])
+
+# Tuple length
+print(len(employee))
+
+# Loop
+for value in employee:
+    print(value)
