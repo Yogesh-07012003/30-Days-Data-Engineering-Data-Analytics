0c253063b8df4b77369e68ccd7b1e3d36f4842e5
Yogesh-07012003 <146987985+Yogesh-07012003@users.noreply.github.com>
2026-09-11 10:01:32 +0530

    Create sets.py

---
+++ Day-02/sets.py
@@ -0,0 +1,19 @@
+numbers = {10, 20, 30, 20, 40, 10}
+
+print(numbers)
+
+# Add
+numbers.add(50)
+
+# Remove
+numbers.remove(20)
+
+print(numbers)
+
+# Set operations
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+print("Union:", a | b)
+print("Intersection:", a & b)
+print("Difference:", a - b)
