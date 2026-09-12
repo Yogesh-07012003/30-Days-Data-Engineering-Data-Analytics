905e800313388e7aa8f8c3d48f6ed8edcf0b0b19
Yogesh-07012003 <146987985+Yogesh-07012003@users.noreply.github.com>
2026-09-12 17:34:13 +0530

    Create string_practice.py

---
+++ Day-03/string_practice.py
@@ -0,0 +1,26 @@
+name = "  kumar  "
+
+# Remove spaces
+name = name.strip()
+
+# Convert to uppercase
+print(name.upper())
+
+# Convert to lowercase
+print(name.lower())
+
+# Capitalize
+print(name.capitalize())
+
+# Replace
+department = "information technology"
+print(department.replace("information", "IT"))
+
+# Split
+full_name = "Arun Kumar"
+parts = full_name.split()
+
+print(parts)
+
+# Join
+print("-".join(parts))
